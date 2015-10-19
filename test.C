b9893bae4c13edc8089521fef322d7a81f920aa5
albertcortez <adennbert@gmail.com>
2015-10-19 14:36:20 -0430

    Working csv small data

--- test.C
+++ test.C
@@ -5,79 +5,50 @@ TEST DE USO DE DATOS DE BIENES Y COMPAÑIAS
 */
 
 #include <stdio.h>
-#include <iostream>
 #include <dbQuery.H>
 #include <string>
-#include <tpl_dynDlist.H>
-#include <tpl_dnode.H>
+#include "fetchDB.H"
 
-typedef struct {
- std::string nombre=" ",
-             rif=" ",
-	     planta=" ",
-             tipo=" ";
-}Company;
+# include <tclap/CmdLine.h>
 
+# include <tpl_dynArray.H>
+
+#include <grafo.H>
+#include <fstream>
 
 
-int main()
+int main(int argc, char *argv[])
 {
-DBConnection conection;
-DBQuery q(conection);
-conection.open("file.txt");
-std::string query;
 
-Aleph::DynDlist <Company> uecon;
-
-
-
-int i=0;
-bool go;
-
-q.exec("SELECT rif, nombre FROM unidadecon_unidad_economica");
-q.reset();
-
-
-
-while(q.hasResult() && q.next())
+	if(argc<2)
 	{
-	Company c;
-	c.nombre=q.getValue(1);
-	c.rif=q.getValue(0);
-	uecon.insert(c);
+	std::cout<<"ERROR, DEBE INGRESAR RIF"<<endl;
+	return 0;
 	}
-q.clear();
 
-for(Aleph::Dnode<Company>::Iterator it(uecon);it.has_curr();it.next())
-{
+	std::string rif;
+	rif=argv[1];
 
-std::cout<<"nombre: "<<it.get_curr()->get_data().nombre<<std::endl<<"rif: "<<it.get_curr()->get_data().rif<<std::endl<<std::endl;
-
-
-}
-/*
-
-std::cout<<endl<<"///////////////////////////PLANTAS/////////////////////////"<<std::endl<<std::endl;
-
-for(i=0;i<100;i++)
-{
-query=(string)"SELECT nombre FROM unidadecon_subunidad_economica WHERE ue_rif="+(string)"\'"+(string)rif[i]+(string)"\'"+(string)" AND tipo_subunidad= 'Se'";
+	Fetch f("file.txt");
 
 
-q.exec(query.c_str());
-go=q.hasResult();
-q.next();
+////////////TEST FROM HERE//////////////
 
-if(go==1)
-{
-planta[i]=q.getValue(0);
-std::cout<<"empresa: "<<nombre[i]<<std::endl<<"planta: "<<planta[i]<<std::endl;
-go=0;
-}
-q.clear();
+	f.unidades_economicas(rif);
+	ofstream file ("Mapa.txt");
+	
+	Mapa mapa(2013, "unidad_economica.csv", "unidadecon_socio.csv",
+   	    "unidadecon_subunidad_economica.csv",
+   	    "produccion_producto.csv", "produccion_insumo.csv", 
+   	    "cmproveedores_proveedorinsumo.csv", "cmproveedores_proveedor.csv",
+   	    "produccion_producto_t_insumo.csv");
 
-}
-*/
+	mapa.save(file);
+
+	
+	
+
+
 return 0;
 
 
