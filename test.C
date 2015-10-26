ed31bc524eeae3a677a3d04b95a988ded8a4ce72
albertcortez <adennbert@gmail.com>
2015-10-26 16:01:07 -0430

    Finally got something

--- test.C
+++ test.C
@@ -10,6 +10,7 @@ TEST DE USO DE DATOS DE BIENES Y COMPAÑIAS
 #include "fetchDB.H"
 
 # include <tpl_dynArray.H>
+# include <tpl_agraph.H>
 
 
 #include <fstream>
@@ -30,33 +31,56 @@ int main(int argc, char *argv[])
 
 	Fetch f("file.txt");
 
-	Data nodo;
+	DynArray<Productos> nodo;
 
+	std::cout<<std::endl<<std::endl<<"BUSCANDO UNIDAD ECONOMICA o PRODUCTO: "<<rif<<" "<<"CON DATOS DE ANIO:"<<anho<<std::endl;
 	nodo=f.search(rif,anho);
-	std::cout<<std::endl<<std::endl<<"BUSCANDOO UNIDAD ECONOMICA o PRODUCTO: "<<rif<<" "<<"CON DATOS DE ANIO:"<<anho<<std::endl;
+
 	
 ////////////TEST FROM HERE//////////////
 
-
-	std::cout<<std::endl<<"----------RESULTS----------"<<std::endl<<std::endl;
-
-	for(DynArray <Productos>::Iterator it(nodo.productos);it.has_curr();it.next())
-	{
-		std::cout<<std::endl<<"Product: "<<it.get_curr().nombre<<std::endl<<"Plant: "<<it.get_curr().subue.nombre<<std::endl<<"Company: "<<it.get_curr().subue.ue.nombre<<std::endl<<"REQUIRES:";
-	   for(DynArray<Arco>::Iterator it0(it.get_curr().arcs);it0.has_curr();it0.next())
-
-		for(DynArray<Productos>::Iterator it1(nodo.insumos);it1.has_curr();it1.next())
-		{
-
-			if(it0.get_curr().insumoID==it1.get_curr().id)
-			std::cout<<std::endl<<"--"<<it1.get_curr().nombre;
 		
-		}
-		
-		std::cout<<std::endl<<std::endl<<"--------------------------------"<<std::endl;
+		bool found=0;
+
+	   for(DynArray<Productos>::Iterator it(nodo);it.has_curr();it.next())
+	    {
+
+			if(!it.get_curr().insumos.is_empty())
+			std::cout<<std::endl<<std::endl<<"----------PRODUCTO:"<<it.get_curr().nombre<<std::endl<<std::endl;
+
+		for(DynArray<Insumos>::Iterator it1(it.get_curr().insumos);it1.has_curr();it1.next())
+			{
+
+			std::cout<<std::endl<<"***INSUMO:"<<it1.get_curr().nombre<<std::endl;
+			for(DynArray<Proveedores>::Iterator it2(it.get_curr().subue.proveedores);it2.has_curr();it2.next())
+				{
+
+					if(it1.get_curr().id==it2.get_curr().insumoID)
+					{
+					std::cout<<std::endl<<"- ORIGEN: "<<it2.get_curr().nombre<<" -- "<<it2.get_curr().paisOrigen<<std::endl; 
+					found=1;
+					}
+			
+				}
+
+			 if(found==0)
+			 std::cout<<std::endl<<"- ORIGEN: NO INFO"<<std::endl;
+			else
+			found=0;
+			
+ 			  
+			}
+
+	    }
+
+	if(nodo.is_empty())
+	std::cout<<std::endl<<"NO SE ENCONTRARON PRODUCTOS CON IMPORTACIONES ASOCIADOS ESE RIF - PRODUCTO"<<std::endl<<std::endl;
+
+	
+	std::cout<<std::endl<<std::endl<<"--------------------------------"<<std::endl;
 
 
-	}
+	
 
 
 	cout<<endl;
