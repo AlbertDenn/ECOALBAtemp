343d727505a65b6d8babec9e89d443d31c851a1e
albertcortez <adennbert@gmail.com>
2015-11-11 08:42:14 -0430

    done with it

--- chainCreate.C
+++ chainCreate.C
@@ -60,7 +60,7 @@ int main(int argc, char *argv[])
 	std::locale loc;
    	rif[0]=std::toupper(rif[0],loc);
 	std::string val;
-	file.open("outjson.json");
+	file.open("out.json");
 
 
 	if(rif[0]=='J'||rif[0]=='G')
@@ -69,55 +69,46 @@ int main(int argc, char *argv[])
 	nodoraiz=root.searchByCode(rif,anho);
 
 
-if(!nodoraiz.is_empty())
-{
-
-	file<<"[";
+	file<<"{\t \"Raiz\":\t[";
 
 	for(DynDlist<UnidadEconomica>::Iterator it(nodoraiz);it.has_curr();)
 	{
 
 		if(!it.get_curr().productos.is_empty() && !it.get_curr().productos[0].insumos.is_empty())
 		{
-			file<<"{ \"Unidad Economica\":\""<<it.get_curr().nombre<<"\",\"RIF\":\""<<it.get_curr().rif<<"\",\"children\":["<<std::endl;
-	
+			file<<"{ \"roleName\":\""<<it.get_curr().nombre+" "+it.get_curr().rif<<"\",\"roleId\":\""<<it.get_curr().rif<<"\",\"children\":["<<std::endl;
+
 			for (DynDlist<Productos>::Iterator it1(it.get_curr().productos);it1.has_curr();it1.next())
 			{
-	
-				file<<"{\"Producto\":\""<<it1.get_curr().nombre<<"\",\"Cantidad Producida\":\""<<it1.get_curr().cantidad<<"\",\"children\":["<<std::endl;
-						
+
+				file<<"{\"roleName\":\""<<it1.get_curr().nombre+" "+it1.get_curr().cod_aran<<"\",\"roleId\":\""<<it1.get_curr().id<<"\",\"children\":["<<std::endl;
 				for(DynDlist<Insumos>::Iterator it0(it1.get_curr().insumos);it0.has_curr();it0.next())
 				{
-	
-				
-					file<<"{\"Insumo\":\""<<std::get<0>(it0.get_curr()).nombre<<"\",\"Cantidad Requerida\":\""<<std::get<0>(it0.get_curr()).cantidad<<"\",\"children\":["<<std::endl;
-				
+
+				file<<"{\"roleName\":\""<<std::get<0>(it0.get_curr()).nombre+" "+std::get<0>(it0.get_curr()).cod_aran<<"\",\"roleId\":\""<<std::get<0>(it0.get_curr()).id<<"\",\"children\":["<<std::endl;
+
 					for(DynDlist<Proveedor>::Iterator it2(std::get<1>(it0.get_curr()));it2.has_curr();it2.next())
 					{
-					file<<"{\"Proveedor\":\""<<it2.get_curr().nombre<<"\",\"Pais\":\""<<it2.get_curr().paisProcedencia<<"\",\"children\":[]}";
-	
+					file<<"{\"roleName\":\""<<it2.get_curr().nombre+" "+it2.get_curr().paisProcedencia<<"\",\"roleId\":\""<<it2.get_curr().paisOrigen<<"\",\"children\":[]}";
+
 					if(it2.get_pos()!=std::get<1>(it0.get_curr()).size()-1 && std::get<1>(it0.get_curr()).size()!=1)
 					file<<","<<std::endl;
-					
-					
-	
-	
-					}	
+
+					}
 					if(it0.get_pos()!=it1.get_curr().insumos.size()-1)
 					file<<"]},"<<std::endl;
 					else
 					file<<"]}"<<std::endl;
-	
-				}	
-		
+
+				}
+
 				if(it1.get_pos()!=it.get_curr().productos.size()-1)
 				file<<"]},"<<std::endl;
 				else
 				file<<"]}"<<std::endl;
 
 			}
-			
-		
+
 			if(it.get_pos()!=nodoraiz.size()-1)
 			file<<"]},"<<std::endl;
 			else
@@ -133,13 +124,9 @@ if(!nodoraiz.is_empty())
 
 	}
 	
-	file<<"]";
-}
-else
-	file<<"[[\"ERROR\"]}"<<std::endl;
-	std::cout<<"NOT FOUND"<<std::endl;
+	file<<"]}";
 	file.close();
-	std::cout<<std::endl<<std::endl;
+	std::cout<<std::endl<<"DONE BABY!"<<std::endl;
 	return 0;
 
 
